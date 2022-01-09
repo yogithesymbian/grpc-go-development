37319fb6ae32ba848a50dbaca6e22163172bb9fb
yogithesymbian <yogirenbox33@gmail.com>
2022-01-09 13:01:58 +0800

    setup server boilerplate

---
+++ greet/greet_server/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"grpc-go-development/greet/greetpb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main()  {
+	fmt.Println("hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen : %w", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %w", err)
+	}
+}
